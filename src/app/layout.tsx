a664e7b1f847ad429b8bbadceb4a5600259ae79a
Vedant Mali <vedantmali05@gmail.com>
2025-07-12 10:15:37 +0000

    Minor changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { BrowserRouter } from 'react-router-dom';
 import './globals.css';
 // import './temp.css';
 
@@ -11,7 +12,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <BrowserRouter>
           {children}
+        </BrowserRouter>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,30 @@
 "use client";
 
+import { getFromLocalStorage, saveToLocalStorage } from '@/utils/browserStorage';
+import { STORAGE_KEYS } from '@/utils/constants';
+import { dummyUsers } from '@/utils/data';
 import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import TaskPage from './tasks/TaskPage';
+import Assistant from './Assistant';
+import Community from './Community';
+import { ToastContainer } from 'react-toastify';
 
 export default function Home() {
+  const users = getFromLocalStorage(STORAGE_KEYS.users);
+  if (!users || users.length === 0) {
+    saveToLocalStorage(STORAGE_KEYS.users, dummyUsers);
+  }
+
   return (
     <main>
-      <div>Hello World</div>
+      <Routes>
+        <Route path="/tasks" element={<TaskPage />} />
+        <Route path="/assistant" element={<Assistant />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="*" element={<Navigate to="/community" />} />
+      </Routes>
+      <ToastContainer />
     </main>
   );
 }
